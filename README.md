a224ce277f66c5948bd1b6490845dd52511a625e
Steven Soule <me@stevensoule.com>
2018-09-02 09:03:41 -0700

    ready for output management

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1. [Basic Setup](https://webpack.js.org/guides/getting-started/#basic-setup)
 2. [Asset Management](https://webpack.js.org/guides/asset-management/)
+3. [Output Management](https://webpack.js.org/guides/output-management/)
--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
   let element = document.createElement('div');
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
   element.classList.add('hello');
-
-  // Add the image to our existing div.
-  let myIcon = new Image();
-  myIcon.src = Icon;
-  myIcon.width = 500;
-  element.appendChild(myIcon);
-
-  console.log(Data);
 
   return element;
 }
