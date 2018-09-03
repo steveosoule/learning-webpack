f9b929bc9bb3352e65d8082558ef61bc2ee4c19b
Steven Soule <me@stevensoule.com>
2018-09-03 11:56:06 -0700

    production done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { cube } from './math.js';
 
+if ( process.env.NODE_ENV !== 'production' ){
+	console.log('Looks like we are in development mode!');
+}
+
 function component() {
   let element = document.createElement('pre');
   element.innerHTML = [
     'Hello webpack!',
-    '8 cubed is equal to ' + cube(8)
+    '5 cubed is equal to ' + cube(5)
   ].join('\n\n');
 
   return element;
