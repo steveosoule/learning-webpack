be6aba9715f9104f509662d4e25e00a21978aa43
Steven Soule <me@stevensoule.com>
2018-09-03 12:11:11 -0700

    code splitting static

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import { cube } from './math.js';
-
-if ( process.env.NODE_ENV !== 'production' ){
-	console.log('Looks like we are in development mode!');
-}
-
 function component() {
   let element = document.createElement('pre');
   element.innerHTML = [
     'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
+    'This is a message.'
   ].join('\n\n');
 
   return element;
