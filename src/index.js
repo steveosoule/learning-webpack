fb60ef52f24240ce5aaa98e921d6ba91e6308218
Steven Soule <me@stevensoule.com>
2018-09-03 12:36:54 -0700

    prepping for lazy-loading

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-function getComponent() {
-	return import ( /* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-		var element = document.createElement('div');
-		var _ = _.default;
-
-		element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-		return element;
-
-	}).catch(error => 'An error occurred while loading the component');
-}
-
-getComponent().then(component => {
-	document.body.appendChild(component);
-});
+import _ from 'lodash';
+function component() {
+  var button = document.createElement('button');
+  var br = document.createElement('br');
+  button.innerHTML = 'Click me and look at the console!';
+  element.appendChild(br);
+  element.appendChild(button);
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default;
+    print();
+  });
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js'
   },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Lazy Loading'
+    })
+  ],
   output: {
     filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
