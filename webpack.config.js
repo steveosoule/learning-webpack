e6ae382dc54c8973f1f76a3ce0c2e481439b33b8
Steven Soule <me@stevensoule.com>
2018-09-03 13:32:41 -0700

    shimming working

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const path = require('path');
 const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'production',
+  entry: {
+    app: './src/index.js'
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Shimming'
+    }),
     new webpack.ProvidePlugin({
       _: 'lodash'
     })
